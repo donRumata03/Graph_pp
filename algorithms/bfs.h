f66d89fd9060906a48da7e11897da66729d17dd0
donRumata <donrumata03@gmail.com>
2020-09-09 22:27:17 +0300

    start bfs development

--- algorithms/bfs.h
+++ algorithms/bfs.h
@@ -6,9 +6,28 @@
 
 // #include "../data_structures/graph_base.h"
 #include "../data_structures/graphs.h"
+#include <queue>
+
 
 template<class Callable, class Graph_Element_T>
-void bfs(const graph_matrix<Graph_Element_T>& graph, Callable callback) {
+void bfs(const graph_matrix<Graph_Element_T>& graph, size_t initial_vertex_index, Callable callback) {
+	if (false) {
+		size_t vertex_number = 0;
+		size_t iteration = 0;
+		Graph_Element_T path_length = Graph_Element_T{};
+
+		// Causes compiler error if your function has false format:
+		callback(vertex, iteration, path_length);
+	}
+
+	assert(initial_vertex_index < graph.n);
+
+	// TODO!
+}
+
+template<class Callable>
+void bfs(const graph_matrix<bool>& graph, size_t initial_vertex_index, Callable callback)
+{
 	if (false) {
 		size_t vertex_number = 0;
 		size_t iteration = 0;
@@ -16,12 +35,29 @@ void bfs(const graph_matrix<Graph_Element_T>& graph, Callable callback) {
 		// Causes compiler error if your function has false format:
 		callback(vertex, iteration);
 	}
+	assert(initial_vertex_index != 0);
+	assert(initial_vertex_index < graph.n);
+	auto n = graph.n;
 
+	std::unique_ptr<size_t> this_wave(new size_t [n]), last_wave(new size_t [n]);
+	size_t this_size = 0, last_size = 0;
 
+	auto pop_this = [&this_wave, &this_size](){ return this_wave[--this_size]; };
+	auto pop_last = [&last_wave, &last_size](){ return last_wave[--last_size]; };
+
+	auto add_this = [&this_wave, &this_size](size_t vert_index){ return this_wave[this_size++] = vert_index; };
+	auto add_last = [&last_wave, &last_size](size_t vert_index){ return last_wave[last_size++] = vert_index; };
+
+	std::vector<bool> viewed_vertexes(n, false);
+	add_last(initial_vertex_index);
+
+	while (last_size) {
+
+	}
 }
 
 template<class Graph_Element_T>
-std::vector<long long> bfs(const graph_matrix<Graph_Element_T>& graph) {
+std::vector<long long> bfs(const graph_matrix<Graph_Element_T>& graph, size_t initial_vertex_index,) {
 	std::vector<long long> res;
 	res.assign(graph.n, -1LL);
 
