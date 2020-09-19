88816ccc94bc745a6562c36cb2e568e7ca2f9044
donRumata <donrumata03@gmail.com>
2020-09-19 10:09:26 +0300

    add ford-bellman algorithm

---
+++ algorithms/ford_bellman.h
@@ -0,0 +1,46 @@
+//
+// Created by Vova on 19.09.2020.
+//
+
+#pragma once
+
+#include "data_structures/graphs.h"
+
+template<class T>
+std::pair<std::vector<T>, std::vector<size_t>> ford_bellman(const graph_matrix<T>& graph, size_t initial_vertex) {
+	using length_t = maximize_type<T>;
+	constexpr auto infinity = graph.infinity;
+	auto n = graph.n;
+
+	std::vector<std::vector<length_t>> d(n); // d[v][i] = the shortest path from initial_vertex to v through i or less edges
+	for (auto& vec : d) vec.resize(n);
+
+	for (size_t v = 0; v < n; ++v) {
+		d[v][0] = infinity;
+	}
+	d[initial_vertex][0] = 0;
+
+	std::vector<size_t> parents(n);
+
+	for (size_t iteration = 1; iteration < n; ++iteration) {
+		for (size_t v = 0; v < n; ++v) {
+			d[v][iteration] = d[v][iteration - 1];
+
+			for (size_t parent_vertex : graph.get_vertex_parents(v)) {
+				// If there are no infinite-length edges
+				d[v][iteration] = min(d[v][iteration], d[parent_vertex][iteration - 1] + graph.get_edge(parent_vertex, v));
+
+			}
+		}
+	}
+
+	std::vector<length_t> res(n);
+	for (size_t v = 0; v < n; ++v) {
+		res[v] = d[v][n - 1];
+	}
+
+	return {
+		res,
+		p
+	};
+}
